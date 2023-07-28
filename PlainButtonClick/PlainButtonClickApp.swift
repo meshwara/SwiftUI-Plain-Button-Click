0ecea894f494731bcb1f6716e5e38c2fb1ea18d7
D. Prameswara <meshwara@gmail.com>
2023-07-29 06:17:59 +0700

    Initial Commit

---
+++ PlainButtonClick/PlainButtonClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlainButtonClickApp.swift
+//  PlainButtonClick
+//
+//  Created by D. Prameswara on 29/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PlainButtonClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
