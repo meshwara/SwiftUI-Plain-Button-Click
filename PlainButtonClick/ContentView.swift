24e27ee07e40876f8cc976288ab29a1adde6d280
D. Prameswara <meshwara@gmail.com>
2023-07-29 13:58:35 +0700

    Initial commit

--- PlainButtonClick/ContentView.swift
+++ PlainButtonClick/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var show = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button {
+            show.toggle()
+        } label: {
+            HStack {
+                Image(systemName: "plus")
+                Text("Add data")
+            }
+            .padding()
+            .frame(maxWidth: .infinity)
+            .contentShape(RoundedRectangle(cornerRadius: 10.0))
+        }
+        .buttonStyle(.plain)
+        .background {
+            RoundedRectangle(cornerRadius: 10.0)
+                .stroke(.primary, lineWidth: 0.2)
         }
         .padding()
+        .sheet(isPresented: $show){
+            Text("Pop Window")
+        }
     }
 }
 
